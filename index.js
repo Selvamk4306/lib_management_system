55f32e352be68fc41ff00197ea29e67512408c44
selvamk4306 <balajiselva4306@gmail.com>
2025-11-13 20:41:48 +0530

    New commit

--- index.js
+++ index.js
@@ -1,21 +1,95 @@
-const express = require('express');
-const app = express();
-const port = 3000
+const express = require("express");
+    const users= require("./data/users.json")
 
-app.use(express.json())
+    const app = express();
+    const port = 3000
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "Hello World"
+    app.use(express.json())
+
+    app.get("/", (req, res) => {
+        res.status(200).json({
+            message: "Home Page"
+        })
     })
-})
 
-// app.all("*", (req, res) => {
-//     res.status(500).json({
-//         message: "Not build yet"
-//     })
-// })
+    /**
+     * Route: /user
+     * Method: GET
+     * Description: Getall the list of users in the public
+     * Access: Public
+     * Paramters: None
+     */
 
-app.listen(port, () => 
-    console.log(`Server is listeneing on port http://localhost:${port}`)
-)
+    app.get("/users", (req, res) => {
+        res.status(200).json({
+            success: true,
+            data: users
+        })
+    })
+
+    app.get("/users/:id", (req, res) => {
+
+        const { id } = req.params;
+        const user = users.find((each) => each.id === id);
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: user
+        })
+    })
+
+    /**
+     * Route: /user
+     * Method: POST
+     * Description: Create / Register new user
+     * Access: Public
+     * Paramters: None
+     */
+
+    app.post("/users", (req, res) => {
+
+        const {id, name, email, subscriptionType, subscriptionStart, subscriptionEnd, hasIssuedBooks, pendingFine} = req.body
+        if(!id || !name || !email || !subscriptionType || !subscriptionStart || !subscriptionEnd || !hasIssuedBooks || pendingFine === undefined){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+        const user = users.find((each) => each.id === id);
+        if(user){
+            return res.status(400).json({
+                success: false,
+                message: `User already exists with id ${id}`
+            })
+        }
+        users.push(
+            id,
+            name,
+            email,
+            subscriptionType,
+            subscriptionStart,
+            subscriptionEnd,
+            hasIssuedBooks,
+            pendingFine
+        )
+        res.status(201).json({
+            success: true,
+            data: "User created successfully "
+        })
+    })
+    // app.all("*", (req, res) => {
+    //     res.status(500).json({
+    //         message: "Not build yet"
+    //     })
+    // })
+
+    app.listen(port, () => 
+        console.log(`Server is listeneing on port http://localhost:${port}`)
+    )
